8a8c9547be651c12b5b775cf57bc2c2bcea40527
Andreww8xx8 <avk@8xx8.ru>
2019-03-18 22:29:04 +0400

    Relationship type fixed

--- src/jupiterone/entities/AccountGroupRelationship.ts
+++ src/jupiterone/entities/AccountGroupRelationship.ts
@@ -4,5 +4,5 @@ export interface AccountGroupRelationship extends RelationshipFromIntegration {
   id?: string;
 }
 
-export const ACCOUNT_GROUP_RELATIONSHIP_TYPE = "google_account_group";
+export const ACCOUNT_GROUP_RELATIONSHIP_TYPE = "google_account_has_group";
 export const ACCOUNT_GROUP_RELATIONSHIP_CLASS = "HAS";
--- src/jupiterone/entities/AccountUserRelationship.ts
+++ src/jupiterone/entities/AccountUserRelationship.ts
@@ -4,5 +4,5 @@ export interface AccountUserRelationship extends RelationshipFromIntegration {
   id?: string;
 }
 
-export const ACCOUNT_USER_RELATIONSHIP_TYPE = "google_account_user";
+export const ACCOUNT_USER_RELATIONSHIP_TYPE = "google_account_has_user";
 export const ACCOUNT_USER_RELATIONSHIP_CLASS = "HAS";

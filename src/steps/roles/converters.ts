e61ffffa92a6610ca824c16d1e23dae23f5990f9
Rick Waterman <rick.waterman@jupiterone.com>
2021-09-17 16:10:47 -0700

    refactor: simplify: createRoleEntity

--- src/steps/roles/converters.ts
+++ src/steps/roles/converters.ts
@@ -4,31 +4,23 @@ import { createIntegrationEntity } from '@jupiterone/integration-sdk-core';
 import { admin_directory_v1 } from 'googleapis';
 import Schema$Role = admin_directory_v1.Schema$Role;
 
-export interface CreateRoleEntityParams {
-  account: { googleAccountId: string };
-  role: Schema$Role;
-}
-
-export function createRoleEntity(data: CreateRoleEntityParams) {
+export function createRoleEntity(role: Schema$Role) {
   return createIntegrationEntity({
     entityData: {
-      source: data,
+      source: role,
       assign: {
         _class: entities.ROLE._class,
-        _key: generateEntityKey(
-          entities.ROLE._type,
-          data.role.roleId as string,
-        ),
+        _key: generateEntityKey(entities.ROLE._type, role.roleId as string),
         _type: entities.ROLE._type,
         // TODO/Rick: Is this the shape we want? I am including more properties rather than fewer for the sake of discussion but can whittle this down
-        id: data.role.roleId as string,
-        name: data.role.roleName,
-        displayName: data.role.roleName as string,
-        roleName: data.role.roleName,
-        roleDescription: data.role.roleDescription,
-        isSystemRole: data.role.isSystemRole,
-        isSuperAdminRole: data.role.isSuperAdminRole,
-        kind: data.role.kind,
+        id: role.roleId as string,
+        name: role.roleName,
+        displayName: role.roleName as string,
+        roleName: role.roleName,
+        roleDescription: role.roleDescription,
+        isSystemRole: role.isSystemRole,
+        isSuperAdminRole: role.isSuperAdminRole,
+        kind: role.kind,
         vendor: 'Google',
       },
     },

f21f4b2a1f894eab246d76cbbeb1cb47a510e343
Gaston Yelmini <gastonyelmini@gmail.com>
2023-12-11 14:48:16 -0300

    INT-9845: limit chrome extensions ingestion time

--- src/gsuite/clients/GSuiteInstalledAppsClient.ts
+++ src/gsuite/clients/GSuiteInstalledAppsClient.ts
@@ -2,6 +2,7 @@ import { chromemanagement_v1 } from 'googleapis';
 
 import { CreateGSuiteClientParams } from './GSuiteClient';
 import { GSuiteChromeManagementClient } from './GSuiteChromeManagementClient';
+import pMap from 'p-map';
 
 type AppTypes = 'EXTENSION' | 'APP' | 'THEME' | 'HOSTED_APP' | 'ANDROID_APP';
 
@@ -24,17 +25,27 @@ export class GSuiteInstalledAppsClient extends GSuiteChromeManagementClient {
   ): Promise<void> {
     const client = await this.getAuthenticatedServiceClient();
 
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+    const oneYearAgoFormatted = oneYearAgo.toISOString().split('T')[0]; // Format date to YYYY-MM-DD
+
     await this.iterateApi(
       async () => {
         return client.customers.reports.countInstalledApps({
           customer: `customers/${this.accountId}`,
-          filter: `app_type=${appType} `,
+          filter: `app_type=${appType} AND latest_profile_active_date>=${oneYearAgoFormatted}`,
         });
       },
       async (data) => {
-        for (const app of data.installedApps || []) {
-          await callback(app);
-        }
+        await pMap(
+          data.installedApps || [],
+          async (app) => {
+            await callback(app);
+          },
+          {
+            concurrency: 5,
+          },
+        );
       },
     );
   }
